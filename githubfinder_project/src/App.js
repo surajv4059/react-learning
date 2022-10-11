41390716ab66b94e82338635089902dcd6c46273
suraj verma <suraj12.116@gmail.com>
2022-10-11 15:14:37 +0530

    .

--- githubfinder_project/src/App.js
+++ githubfinder_project/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
-  fooo = () => "in class";
   render() {
-    const name = "Suraj";
-    const foo = () => "Bar";
-    const loading = false;
-    const showName = true;
-
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
-        <h1>Hello {name.toUpperCase()} </h1>
-        <h1>Hello {foo()} </h1>
-        <h1>Hello {this.fooo()} </h1>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
