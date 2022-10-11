a4541c99eedfece35063ec42b585009be55a58c6
suraj verma <suraj12.116@gmail.com>
2022-10-11 12:35:50 +0530

    expressions and conditionals

--- githubfinder_project/src/App.js
+++ githubfinder_project/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  fooo = () => "in class";
+  render() {
+    const name = "Suraj";
+    const foo = () => "Bar";
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className="App">
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <h1>Hello {name.toUpperCase()} </h1>
+        <h1>Hello {foo()} </h1>
+        <h1>Hello {this.fooo()} </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
